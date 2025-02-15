dcd6ed09b0fbda55c24af7a51d1b6c6c39fb095e
Ishaan Ratanshi <i.ratanshi1@gmail.com>
2025-02-15 11:41:17 -0700

    started backend

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes import router
+
+app = fastapi.FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
